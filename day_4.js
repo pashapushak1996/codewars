91ec5479aa546f9c980ada13788ca6bc8fef3ccc
pashapushak1996 <pashapushak1996@gmail.com>
2021-11-27 23:18:45 +0200

    day_4

--- day_4.js
+++ day_4.js
@@ -159,3 +159,24 @@ let b = alphanumeric('i4N2Gi!Vs1XIlKW3ee6xlyYvoMxgg');
 const sortWord = (string = '') => string.toLowerCase().split('').sort().join('');
 
 const isAnagram = (test, original) => sortWord(test) === sortWord(original);
+
+//Given a list of integers values, your job is to return the sum of the values; however, if the same integer value appears multiple times in the list, you can only count it once in your sum.
+
+function uniqueSum(lst) {
+    const result = [];
+
+    lst.forEach((el) => {
+        if (!result.includes(el)) {
+            result.push(el)
+        }
+    });
+
+    return lst.length ? result.reduce((acc, curr) => acc + curr) : null;
+}
+
+//Second solution
+function uniqueSumTwo(lst) {
+    return [...new Set(lst)].reduce((a, b) => a + b, null);
+}
+
+uniqueSum([1, 3, 8, 1, 8]);
